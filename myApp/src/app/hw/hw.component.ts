a6b088e71c216fb0369035dd28809d22aeffb20a
TechSulai786 <techsulai786@outlook.com>
2024-01-16 19:09:32 +0530

    added

---
+++ myApp/src/app/hw/hw.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hw',
+  standalone: true,
+  imports: [],
+  templateUrl: './hw.component.html',
+  styleUrl: './hw.component.css'
+})
+
+export class HwComponent {
+
+}

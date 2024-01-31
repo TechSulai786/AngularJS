75c45eb6cd80fa42e6f1d32d8fd4e8e024ff08c1
TechSulai786 <techsulai786@outlook.com>
2024-01-31 14:27:51 +0530

    updated

--- Asp-Angular-crud/obj/Debug/net8.0/Asp-Angular-crud.AssemblyInfo.cs
+++ Asp-Angular-crud/obj/Debug/net8.0/Asp-Angular-crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp-Angular-crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684a22a31e218b452bf2e26960e716e1a711ebe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685820c68cc1b62eeaf6cd71eac9b360b918c8e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp-Angular-crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp-Angular-crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
